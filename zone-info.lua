79470909dc2ece3e568d13bc1a63ad89e89bfb53
michael stopps <105889094+michael-stopps@users.noreply.github.com>
2026-06-18 20:39:27 -0500

    refactor additional variables

--- zone-info.lua
+++ zone-info.lua
@@ -30,7 +30,7 @@ local categoryObj
 -- INIT
 frame:RegisterEvent("ADDON_LOADED")
 frame:SetScript("OnEvent", function(self, event, arg1)
-    if event == "ADDON_LOADED" and arg1 == "ZoneInfo" then
+    if event == "ADDON_LOADED" and arg1 == "zone-info" then
         -- Initialize Database
         ZoneInfoDB = ZoneInfoDB or {}
         local defaults = {
